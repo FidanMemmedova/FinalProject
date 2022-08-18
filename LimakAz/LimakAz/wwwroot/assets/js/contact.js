80e4f82983c3fa9b64e3f1f6d2524b3faeeaecde
FidanMemmedova <5TAWDL9@code.edu.az>
2022-08-18 23:33:51 +0400

    first commit

---
+++ LimakAz/LimakAz/wwwroot/assets/js/contact.js
@@ -0,0 +1,15 @@
+﻿//Contact page Show/Hide Map 
+function mapFunction() {
+    let contactMapHandler = document.querySelectorAll(".test");
+    console.log('--->', contactMapHandler);
+    contactMapHandler.forEach((map) => {
+        if (map.style.display === "none") {
+            map.style.display = "block";
+        }
+        else {
+            map.style.display = "none";
+        }
+    })
+   
+}
+    
